dc6781619946b913e355acf65b2be4d57212cc5e
Nick Paladino <nickp@ncpholdings.org>
2018-05-20 02:19:41 -0500

    Small adjustment

--- day9.rs
+++ day9.rs
@@ -4,11 +4,11 @@ use std::fs::File;
 fn main()
 {
     let mut input_file = File::open("input_day9.txt").expect("cant find file");
-    let mut input : String = String::new();
+    let mut input = String::new();
 
     input_file.read_to_string(&mut input).expect("Could not read into string");
 
-    println!("{}", input);
+    //println!("{}", input);
 
-
+    
 }
